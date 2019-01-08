decd4915bc22bb3fdd88b97ad042953fb3a4911d
Gbolahan Fawale <gbmilla@gmail.com>
2019-01-08 04:03:10 +0100

    github star

--- src/components/Footerr.vue
+++ src/components/Footerr.vue
@@ -1,34 +1,52 @@
 <template>
-	<div id = "footerr">
-		   <!-- Footer -->
+  <div id="footerr">
+    <!-- Footer -->
 
     <footer class="py-5 pt-5 mt-5 bg-dark">
       <div class="container">
-        <div class = "row">
-          <div class="col-lg-4 mb-4">
-        <p class="m-0 text-center text-footer">Made with love in <a href="https://figma.com">Figma</a> and site with <a href="https://vuejs.org">Vue.js</a></p>
-      </div>
+        <div class="d-flex justify-content-between flex-wrap align-content">
 
-       <div class="col-lg-4 mb-4">
-        <p class="m-0 text-center text-footer">Project by  <a href="https://twitter.com/GbMillz">Gbolahan Taoheed Fawale</a></p>
-      </div>
+          <div class="m-3 text-center text-footer">Made with love in
+            <a href="https://figma.com">Figma</a> and site with
+            <a href="https://vuejs.org">Vue.js</a>
+          </div>
 
-       <div class="col-lg-4 mb-4">
-        <p class="m-0 text-center text-footer"><a href="https://twitter.com/Jolloficons">Jolloficons on Twitter</a></p>
+          <div class="m-3 text-center text-footer">Project by
+            <a href="https://twitter.com/GbMillz">Gbolahan Taoheed
+              Fawale
+            </a>
+          </div>
 
-      </div>
+          <div class="m-3 text-center text-footer">
+            <a href="https://twitter.com/Jolloficons">Jolloficons on Twitter</a>
+          </div>
+
+          <div class="m-3">
+            <a class="github-button"
+               href="https://github.com/gbmillz/jolloficons"
+               data-size="small"
+               data-show-count="true"
+               aria-label="Star gbmillz/jolloficons on GitHub">
+              Star
+            </a>
+          </div>
+
         </div>
-         <!-- /.row -->
-          <p class="m-0 text-center text-footer">Copyright &copy; Jolloficons 2018</p>
+        <!-- /.row -->
+
+        <div class="my-3 mx-auto text-center text-footer">
+          Copyright &copy; Jolloficons 2018
+        </div>
+
       </div>
       <!-- /.container -->
     </footer>
 
-	</div>
+  </div>
 </template>
 
 
- <script>
+<script>
 export default {
   name: "Footerr"
 };
